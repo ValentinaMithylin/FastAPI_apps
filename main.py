765a007045bc83fd6785e9a9071561cd4ce4fe3d
ValentinaMithylin <mithylin15@gmail.com>
2025-01-05 16:06:21 +0600

    updated files

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", response_class=FileResponse)
 async def read_root():
@@ -14,3 +15,18 @@ async def read_root():
 @app.get("/home", response_class=FileResponse)
 async def read_home():
     return FileResponse("templates/home.html")
+
+
+@app.get("/resume", response_class=FileResponse)
+async def read_resume():
+    return FileResponse("templates/resume.html")
+
+
+@app.get("/portfolio", response_class=FileResponse)
+async def read_portfolio():
+    return FileResponse("templates/portfolio.html")
+
+
+@app.get("/contact", response_class=FileResponse)
+async def read_contact():
+    return FileResponse("templates/contact.html")
